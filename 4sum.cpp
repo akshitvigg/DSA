c0fe74d8d4b902b90d1c48b85242c122fdb02f10
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-06 20:50:41 +0530

    Update 4sum.cpp

--- 4sum.cpp
+++ 4sum.cpp
@@ -5,21 +5,18 @@
 using namespace std;
 
 vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    int n = nums.size(); //size of the array
+    int n = nums.size();
     vector<vector<int>> ans;
 
-    // sort the given array:
     sort(nums.begin(), nums.end());
 
-    //calculating the quadruplets:
+
     for (int i = 0; i < n; i++) {
-        // avoid the duplicates while moving i:
+
         if (i > 0 && nums[i] == nums[i - 1]) continue;
         for (int j = i + 1; j < n; j++) {
-            // avoid the duplicates while moving j:
+        
             if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-            // 2 pointers:
             int k = j + 1;
             int l = n - 1;
             while (k < l) {
@@ -32,7 +29,7 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
                     ans.push_back(temp);
                     k++; l--;
 
-                    //skip the duplicates:
+        
                     while (k < l && nums[k] == nums[k - 1]) k++;
                     while (k < l && nums[l] == nums[l + 1]) l--;
                 }

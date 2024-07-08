725fe4230ff9eeed936865f7bcf6ce39cf6feb17
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-08 20:37:41 +0530

    Update kokoEatingBananas.cpp

--- kokoEatingBananas.cpp
+++ kokoEatingBananas.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int findMax(vector<int> &v) {
     int maxi = INT_MIN;
     int n = v.size();
-    //find the maximum:
+  
     for (int i = 0; i < n; i++) {
         maxi = max(maxi, v[i]);
     }
@@ -17,7 +17,7 @@ int findMax(vector<int> &v) {
 int calculateTotalHours(vector<int> &v, int hourly) {
     int totalH = 0;
     int n = v.size();
-    //find total hours:
+
     for (int i = 0; i < n; i++) {
         totalH += ceil((double)(v[i]) / (double)(hourly));
     }
@@ -27,7 +27,7 @@ int calculateTotalHours(vector<int> &v, int hourly) {
 int minimumRateToEatBananas(vector<int> v, int h) {
     int low = 1, high = findMax(v);
 
-    //apply binary search:
+
     while (low <= high) {
         int mid = (low + high) / 2;
         int totalH = calculateTotalHours(v, mid);

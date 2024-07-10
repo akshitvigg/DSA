c51ad970d78c7f9140476c19cbb5f03ec4752b15
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-10 19:35:09 +0530

    Create Nthroot.java

---
+++ Nthroot.java
@@ -0,0 +1,40 @@
+
+
+
+import java.util.*;
+
+public class ak {
+    public static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+
+    public static int NthRoot(int n, int m) {
+     the answer space:
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 27;
+        int ans = NthRoot(n, m);
+        System.out.println("The answer is: " + ans);
+    }
+}
+

eb97a28527cd16c890a4fd77f5fdf82931d66266
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-11 18:53:53 +0530

    Create SmallestDivi.cpp

---
+++ SmallestDivi.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int sumByD(vector<int> &arr, int div) {
+    int n = arr.size();
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += ceil((double)(arr[i]) / (double)(div));
+    }
+    return sum;
+}
+
+int smallestDivisor(vector<int>& arr, int limit) {
+    int n = arr.size();
+    if (n > limit) return -1;
+    int low = 1, high = *max_element(arr.begin(), arr.end());
+
+   
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (sumByD(arr, mid) <= limit) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int limit = 8;
+    int ans = smallestDivisor(arr, limit);
+    cout << "The minimum divisor is: " << ans << "\n";
+    return 0;
+}
+

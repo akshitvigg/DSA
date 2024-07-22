921c3402efd5958e2981470354876ace49ba124b
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-22 22:10:16 +0530

    Create InsertBeforeHEad.cpp

---
+++ InsertBeforeHEad.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+class Node {
+public:
+    int data;        
+    Node* next;
+    Node* back;
+
+    
+    Node(int data1, Node* next1, Node* back1) {
+        data = data1;
+        next = next1;
+        back=back1;
+    }
+
+  
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+        back= nullptr;
+    }
+};
+
+Node* convertArr2dll(vector<int>arr){
+  if(arr.empty()){
+    return nullptr;
+  }
+  Node* head = new Node(arr[0]);
+  Node* mover = head;
+  for(int i =1; i<arr.size();i++){
+    Node* temp = new Node(arr[i]);
+    mover->next = temp;
+    temp->back = mover; 
+    mover = temp;
+    
+  }  
+  return head;
+}
+
+void  print(Node* head){
+  while(head!=nullptr){
+    cout<<head->data<<" ";
+    head= head->next;
+  }
+  cout<<endl;
+  
+}
+
+Node* insertBeforehead(Node* head, int val){
+  Node* newHead = new Node(val,head,nullptr);
+  head->back = newHead;
+
+  return newHead;
+  
+}
+
+int main() {
+
+  vector<int>arr = {7,4,3,2};
+  Node* head = convertArr2dll(arr);
+
+  head = insertBeforehead(head,50);
+  print(head);
+
+    
+
+    return 0;
+}

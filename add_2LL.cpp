80c87128dc4e93796bea307371c829ced5e6c9cb
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-26 23:11:09 +0530

    Create add_2LL.cpp

---
+++ add_2LL.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyNode = new ListNode(0);
+        ListNode* curr = dummyNode;
+        int carry = 0;
+        
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+            int sum = carry;
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+        
+        return dummyNode->next;
+    }
+};

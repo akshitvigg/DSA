1582d1824a80690b4785da3c98921aa42364a80a
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-07-30 18:34:02 +0530

    Create QueueUSingLL.cpp

---
+++ QueueUSingLL.cpp
@@ -0,0 +1,27 @@
+void MyQueue:: push(int x)
+{
+    QueueNode* temp = new QueueNode(x);
+    if(front==nullptr){
+        front=temp;
+        rear = temp;
+    }else{
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+    if(front==nullptr){
+        cout<<"queue is empty"<<endl;
+        
+    }else{
+    int value = front->data;
+    QueueNode* temp = front;
+    front = front->next;
+    delete(temp);
+    return value;
+    }
+  
+}

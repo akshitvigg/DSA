ab476e590e870829a1eb4c6985f8586e9072dbc6
Akshit-Vig01 <156829501+Akshit-Vig01@users.noreply.github.com>
2024-09-11 19:47:23 +0530

    Update Longest_rep_char_replacement.cpp

--- 2pointers&Sliding-Window/Longest_rep_char_replacement.cpp
+++ 2pointers&Sliding-Window/Longest_rep_char_replacement.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int maxl=0, l=0, r=0,maxfr=0;
+        int hash[26]={0};
+        while(r<s.size()){
+            hash[s[r]-'A']++;
 
+            if(hash[s[r]-'A']>maxfr){
+                maxfr = hash[s[r]-'A'];
+
+            }if((r-l+1)-maxfr>k){
+                hash[s[l]-'A']--;
+                l++;
+            }
+            maxl = max(r-l+1,maxl);
+            r++;
+
+        }
+        return maxl;
+    }
+};

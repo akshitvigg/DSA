6fc9ac8d7ba789ccdf0453945b5fa37c682735df
Akshit Vig <akshitvig48@gmail.com>
2024-09-22 18:27:25 +0530

    Create MorrisTraversal inorder.cpp

---
+++ Trees/MorrisTraversal inorder.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int >res;
+        TreeNode* curr = root;
+        TreeNode* pre;
+        while(curr!=nullptr){
+            if(curr->left==nullptr){
+                res.push_back(curr->val);
+                curr = curr->right;
+            }else{
+                pre = curr->left;
+                while(pre->right!=nullptr){
+                    pre = pre->right;
+                }
+                pre->right = curr;
+                TreeNode* temp  = curr;
+                curr = curr->left;
+                temp->left = nullptr;
+            }
+        }
+        return res;
+    }
+};  

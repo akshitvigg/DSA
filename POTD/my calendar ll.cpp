2ce244ae2233636dd518890791b60f229ed3713b
Akshit Vig <akshitvig48@gmail.com>
2024-09-28 15:56:10 +0530

    Create my calendar ll.cpp

---
+++ POTD/my calendar ll.cpp
@@ -0,0 +1,30 @@
+class MyCalendarTwo {
+    vector<pair<int,int>> doublebookings;
+    vector<pair<int,int>> bookings;
+public:
+    bool checkOverlap(int start1,int end1, int start2, int end2){
+        return max(start1, start2) < min(end1, end2);
+    }
+    pair<int, int> findoverlapregion(int start1,int end1, int start2, int end2){
+        return {max(start1, start2), min(end1, end2)};
+    }
+
+    MyCalendarTwo() {}
+
+    bool book(int start, int end) {
+        for(pair<int, int>region : doublebookings){
+            if(checkOverlap(region.first, region.second, start, end)){
+                return false;
+            }
+        }
+
+        for(pair<int,int>bookin : bookings){
+            if(checkOverlap(bookin.first, bookin.second, start, end)){
+                doublebookings.push_back(findoverlapregion(bookin.first, bookin.second, start, end));
+            }
+           
+        }
+         bookings.push_back({start,end});
+            return true;
+    }
+};

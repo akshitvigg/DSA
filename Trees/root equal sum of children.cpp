85d8153ec21643d510285c2e198bcbed1f7d5789
Akshit Vig <akshitvig48@gmail.com>
2024-09-28 19:47:49 +0530

    Create root equal sum of children.cpp

---
+++ Trees/root equal sum of children.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if(root->left->val+root->right->val==root->val){
+            return true;
+        }
+        return false;
+        
+    }
+};

d9fb54c54a95cfb6217cd7b1cc928575c8aced21
Akshit Vig <akshitvig48@gmail.com>
2024-09-29 23:48:30 +0530

    Create All O`one Data Structure.cpp

---
+++ POTD/All O`one Data Structure.cpp
@@ -0,0 +1,53 @@
+class AllOne {
+public:
+
+    void inc(string key) {
+        
+    
+        if (!bucketOfKey.count(key))
+
+        auto next = bucketOfKey[key], bucket = next++;
+        if (next == buckets.end() || next->value > bucket->value + 1)
+            next = buckets.insert(next, {bucket->value + 1, {}});
+        next->keys.insert(key);
+        bucketOfKey[key] = next;
+        
+
+        bucket->keys.erase(key);
+        if (bucket->keys.empty())
+            buckets.erase(bucket);
+    }
+
+    void dec(string key) {
+
+        if (!bucketOfKey.count(key))
+            return;
+
+
+        auto prev = bucketOfKey[key], bucket = prev--;
+        bucketOfKey.erase(key);
+        if (bucket->value > 1) {
+            if (bucket == buckets.begin() || prev->value < bucket->value - 1)
+                prev = buckets.insert(bucket, {bucket->value - 1, {}});
+            prev->keys.insert(key);
+            bucketOfKey[key] = prev;
+        }
+
+        bucket->keys.erase(key);
+        if (bucket->keys.empty())
+            buckets.erase(bucket);
+    }
+
+    string getMaxKey() {
+        return buckets.empty() ? "" : *(buckets.rbegin()->keys.begin());
+    }
+    
+    string getMinKey() {
+        return buckets.empty() ? "" : *(buckets.begin()->keys.begin());
+    }
+
+private:
+    struct Bucket { int value; unordered_set<string> keys; };
+    list<Bucket> buckets;
+    unordered_map<string, list<Bucket>::iterator> bucketOfKey;
+};

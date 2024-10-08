7862a4d3bce51a14778b1f19d8b7a094deaac121
Akshit Vig <akshitvig48@gmail.com>
2024-10-08 18:04:49 +0530

    Create Valid BST.cpp

---
+++ Trees/BST/Valid BST.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, INT_MIN, INT_MAX);
+    }
+    bool isValidBST(TreeNode* root, long minval , long maxval){
+        if(root==nullptr) return true;
+        if(root->val>=maxval || root->val<=minval)return false;
+        return isValidBST(root->left,minval,root->val)&& isValidBST(root->right,root->val,maxval);
+    }
+};

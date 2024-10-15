d8d2dd2333904eb55eef74c8ef263f1043d790cc
Akshit Vig <akshitvig48@gmail.com>
2024-10-15 21:36:29 +0530

    Create seperate white & black balls.cpp

---
+++ POTD/seperate white & black balls.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        int n = s.size();
+
+        long long swap = 0;
+        int black= 0;
+
+        for(int  i = 0; i<n; i++){
+            if(s[i] == '1'){
+                black++;
+             
+            }else{//if (s[i]=='0)
+                swap+=black;
+            }
+           
+         
+            
+        }
+        return swap;
+        
+    }
+};

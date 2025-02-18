35459de963583b6b1583d90dcc491e80730ab0b7
Akshit Vig <akshitvig48@gmail.com>
2025-02-18 22:51:52 +0530

    Create PracticeA - Welcome to AtCoder .cpp

---
+++ CP/AtCoder/PracticeA - Welcome to AtCoder .cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  char s[100];
+
+  cin >> a;
+  cin >> b >> c;
+
+  cin >> s;
+
+  cout << a + b + c << " " << s << endl;
+  return 0;
+}

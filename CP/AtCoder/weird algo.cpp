574a82fa0b471c75910e118acdf9a757a2b1c20c
Akshit Vig <akshitvig48@gmail.com>
2025-03-19 19:05:58 +0530

    Create weird algo.cpp

---
+++ CP/AtCoder/weird algo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+ 
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+ 
+  long long n;
+  cin >> n;
+ 
+  cout << n ;
+  while (n != 1) {
+ 
+    if (n % 2 == 0) {
+      n = n / 2;
+      cout << " " << n;
+    } else {
+      n = (n * 3) + 1;
+      cout << " " << n;
+    }
+  }
+}
